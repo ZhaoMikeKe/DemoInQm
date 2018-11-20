862d7cae7bc84027b3d3cffae1ce577da4454416
zhaoke <514332859@qq.com>
2018-11-20 17:11:28 +0800

    bottomnavigation

--- bottomnavigation/src/main/java/com/zhaoke/bottomnavigation/MainActivity.java
+++ bottomnavigation/src/main/java/com/zhaoke/bottomnavigation/MainActivity.java
@@ -2,10 +2,14 @@ package com.zhaoke.bottomnavigation;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import java.lang.reflect.Field;
 
 public class MainActivity extends AppCompatActivity {
 
