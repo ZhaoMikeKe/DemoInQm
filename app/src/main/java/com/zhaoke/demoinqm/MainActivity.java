89b56475923230ccf56038405811c2cb20fee656
zhaoke <514332859@qq.com>
2018-11-20 14:46:57 +0800

    t

--- app/src/main/java/com/zhaoke/demoinqm/MainActivity.java
+++ app/src/main/java/com/zhaoke/demoinqm/MainActivity.java
@@ -1,13 +1,53 @@
 package com.zhaoke.demoinqm;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import me.jessyan.autosize.internal.CustomAdapt;
+
+public class MainActivity extends AppCompatActivity implements CustomAdapt, View.OnClickListener {
+
+    private TextView name;
+    private Button login;
+    private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    @Override
+    public boolean isBaseOnWidth() {
+        return true;
+    }
+
+    @Override
+    public float getSizeInDp() {
+        return 360;
+    }
+
+    private void initView() {
+        name = (TextView) findViewById(R.id.name);
+        login = (Button) findViewById(R.id.login);
+
+        login.setOnClickListener(this);
+        register = (Button) findViewById(R.id.register);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login:
+
+                break;
+            case R.id.register:
+                break;
+        }
     }
 }
