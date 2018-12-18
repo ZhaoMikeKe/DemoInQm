6993801183917b6a99650b67a41ffc70f5b94f48
zhaoke <514332859@qq.com>
2018-12-18 10:58:45 +0800

    图片展示项目

--- ninepicture/src/main/java/com/zhaoke/ninepicture/App.java
+++ ninepicture/src/main/java/com/zhaoke/ninepicture/App.java
@@ -10,6 +10,6 @@ import android.app.Application;
 public class App extends Application {
     @Override
     public void onCreate() {
-        super.onCreate();
+        super.onCreate();//test
     }
 }
