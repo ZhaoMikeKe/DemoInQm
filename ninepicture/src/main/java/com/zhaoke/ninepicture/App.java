ca4b65ed41c9df62bf972b8f8b47c3397f8cc260
zhaoke <514332859@qq.com>
2018-12-18 10:59:27 +0800

    test2

--- ninepicture/src/main/java/com/zhaoke/ninepicture/App.java
+++ ninepicture/src/main/java/com/zhaoke/ninepicture/App.java
@@ -10,6 +10,6 @@ import android.app.Application;
 public class App extends Application {
     @Override
     public void onCreate() {
-        super.onCreate();//test1
+        super.onCreate();//test1 test2
     }
 }
